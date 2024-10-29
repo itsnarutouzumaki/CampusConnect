3b19f319c8001cb71df079079d93a87d2a999b36
ABHI002684 <abhishek002684@gmail.com>
2024-10-29 21:46:20 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const express=require('express');
 const bodyParser=require('body-parser');
 const cors=require('cors');
 const app=express();
+const dotenv=require('dotenv');
+dotenv.config();
 const PORT=process.env.PORT || 1000;
 //routes
 const courseRouter=require('./Routes/Course');
@@ -22,8 +24,11 @@ app.get('/',(req,res)=>res.json({message:"this is a home route"}));
 //using user's router
 app.use('/backend/course',courseRouter);
 
+let username=process.env.Database_UserName;
+let password=process.env.Database_password;
 mongoose.connect(
     "mongodb+srv://abhishek002684:Abhi%401204@cluster0.dohre.mongodb.net/",{
+    `mongodb+srv://${username}:${password}@cluster0.dohre.mongodb.net/`,{
         useNewUrlParser: true,
         useUnifiedTopology: true,
         dbName:"CampusConnect"
