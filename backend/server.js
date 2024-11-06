aa49334471e1f6213329b1e6a7012135af018cb0
ABHI002684 <abhishek002684@gmail.com>
2024-11-07 00:01:02 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,15 @@ const bodyParser=require('body-parser');
 const cors=require('cors');
 const app=express();
 const dotenv=require('dotenv');
+const path=require('path');
 dotenv.config();
 const PORT=process.env.PORT || 1000;
+//middlewares
+const upload=require('./Middlewares/upload');
 //routes
 const courseRouter=require('./Routes/Course');
 const assignmentRouter=require('./Routes/Assignment');
+const chapterRouter=require('./Routes/Chapter');
 app.use(bodyParser.json());
 
 //using cors to connect backend and frontend
@@ -26,6 +30,12 @@ app.use('/backend/course',courseRouter);
 
 //using assignment router
 app.use('/backend/assignment',assignmentRouter);
+
+//using chapter router
+app.use('/backend/chapter',chapterRouter);
+
+// Serve the uploads directory
+// app.use('/uploads', express.static('uploads'));
 
 let username=process.env.Database_UserName;
 let password=process.env.Database_password;
