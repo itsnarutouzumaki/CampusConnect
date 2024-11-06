ae693e3a98d18a54812f34e1eb90617460330a18
somenath singh <144797336+itsnarutouzumaki@users.noreply.github.com>
2024-11-06 19:09:20 +0530

    teacher profile and login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "./components/Students/Navbar";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import "./app.css";
 import LoginSignupForm from "./components/Students/LoginSignup";
 import Course from "./components/Students/Course";
@@ -8,23 +8,37 @@ import Assignment from "./components/Students/Assignment";
 import CopyrightFooter from "./components/Students/Footer";
 import ContactUsForm from "./components/Students/Contact";
 import HomeScreen from "./components/Students/Dashboard";
+import TeacherLoginForm from "./components/Teacher/Login";
+import Profile from "./components/Teacher/Profile";
 
 const App = () => {
   return (
     <Router>
-      <Navbar />
-      <div className="min-h-[81vh] w-full">
-      <Routes>
-        <Route path="/" element={<LoginSignupForm />} />     
-        <Route path="/loginsignup" element={<LoginSignupForm />} />
-        <Route path="/course" element={<Course />} />
-        <Route path="/assignment" element={<Assignment />} />
-        <Route path="/contactUs" element={<ContactUsForm />} />
-        <Route path="/dashboard" element={<HomeScreen />} />
-      </Routes>
-      </div>
-      <CopyrightFooter/>
+      <MainContent />
     </Router>
+  );
+};
+
+const MainContent = () => {
+  const location = useLocation();
+
+  return (
+    <>
+      {location.pathname !== '/teacher/login' && <Navbar />}
+      <div className="min-h-[81vh] w-full">
+        <Routes>
+          <Route path="/" element={<LoginSignupForm />} />
+          <Route path="/loginsignup" element={<LoginSignupForm />} />
+          <Route path="/course" element={<Course />} />
+          <Route path="/assignment" element={<Assignment />} />
+          <Route path="/contactUs" element={<ContactUsForm />} />
+          <Route path="/dashboard" element={<HomeScreen />} />
+          <Route path="/teacher/login" element={<TeacherLoginForm />} />
+          <Route path="/teacher/profile" element={<Profile />} />
+        </Routes>
+      </div>
+      <CopyrightFooter />
+    </>
   );
 };
 
