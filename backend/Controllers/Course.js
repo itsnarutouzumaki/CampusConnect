30b52be611dafcad99d787e19ac476def6f237a6
ABHI002684 <abhishek002684@gmail.com>
2024-11-07 20:03:00 +0530

    updated course api's

--- backend/Controllers/Course.js
+++ backend/Controllers/Course.js
@@ -1,39 +1,76 @@
-const Courses=require('../Models/course');
+const Courses = require("../Models/course");
 
 // adding a course
-const addCourse= async (req,res) =>{
-    let {name,
-        code,
-        description,
-        credit,
-        duration,
-        startDate,
-        endDate,
-        enrollmentCount,
-        createdAt}=req.body;
+const addCourse = async (req, res) => {
+  let {
+    name,
+    code,
+    description,
+    credit,
+    duration,
+    startDate,
+    endDate,
+    enrollmentCount,
+    createdAt,
+  } = req.body;
 
-        try{
-            let course= await Courses.create({name,
-                code,
-                description,
-                credit,
-                duration,
-                startDate,
-                endDate,
-                enrollmentCount,
-                createdAt});
+  try {
+    let course = await Courses.create({
+      name,
+      code,
+      description,
+      credit,
+      duration,
+      startDate,
+      endDate,
+      enrollmentCount,
+      createdAt,
+    });
 
-            res.json({message:"course added successfully",course});
-        }catch(err){
-            res.json({message:err.message});
-        }
-}
-
+    res.json({ message: "course added successfully", course });
+  } catch (err) {
+    res.json({ message: err.message });
+  }
+};
 
 // get course details
-const getCourse= async (req,res)=>{
-    let course=await Courses.find().sort({createdAt:-1});
-    res.json({message:"all courses",course});
-}
+const getCourse = async (req, res) => {
+  let course = await Courses.find().sort({ createdAt: -1 });
+  res.json({ message: "all courses", course });
+};
 
-module.exports={addCourse,getCourse};
+// update a course
+const updateCourse = async (req, res) => {
+  try {
+    const courseId = req.params.id;
+
+    const course = await Courses.findByIdAndUpdate(courseId, req.body, {
+      new: true
+    });
+    if (!course) {
+      return res.json({ message: "course doesn't exist" });
+    }
+
+    return res.json({ message: "course has been updated", course });
+  } catch (err) {
+    return res.json({ message: err.message });
+  }
+};
+
+// delete a chapter
+const deleteCourse = async (req, res) => {
+  try {
+    const courseId = req.params.id;
+    const course = await Courses.findByIdAndDelete(courseId);
+
+    if (!course) {
+      return res.json({ message: "course doesn't exist" });
+    }
+
+    return res.json({ message: "course deleted successfully", course });
+  } catch (err) {
+    return res.json({ message: err.message });
+  }
+};
+
+module.exports = { addCourse, getCourse, updateCourse, deleteCourse };
