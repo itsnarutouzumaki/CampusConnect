2807e1c18cc9b78f5e0caa7dee3c24a0390128b9
kamal20247 <147083931+kamalgurjar7@users.noreply.github.com>
2024-11-09 18:22:16 +0530

    all modals are customized

--- frontend/src/components/Modal/RemoveCourse.modal.jsx
+++ frontend/src/components/Modal/RemoveCourse.modal.jsx
@@ -1,24 +1,31 @@
-import React, { useEffect } from 'react'
-import ReactDOM from 'react-dom'
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom';
 
 const RemoveCourse = ({ closeModal }) => {
   useEffect(() => {
-    document.body.style.overflowY = 'hidden'
+    document.body.style.overflowY = 'hidden';
     return () => {
-      document.body.style.overflowY = 'scroll'
-    }
-  }, [])
+      document.body.style.overflowY = 'scroll';
+    };
+  }, []);
 
   return ReactDOM.createPortal(
     <div className="fixed top-0 left-0 w-screen h-screen bg-gray-800 bg-opacity-50 flex justify-center items-center z-50" onClick={closeModal}>
       <div className="bg-gray-900 text-white p-6 rounded-lg max-w-lg w-full sm:w-[500px] z-50 flex flex-col text-center shadow-lg hover:border-2 hover:border-white hover:shadow-2xl hover:shadow-gray-500 transition-all" onClick={(e) => e.stopPropagation()}>
-        <h2 className="text-2xl font-bold mb-4">RemoveCourse</h2>
-        <p className="text-sm mb-4">Subscribe to our newsletter and never miss our designs, latest news, etc. Our newsletter is sent once a week, every Monday.</p>
-        <button className="bg-blue-500 text-white py-2 px-4 rounded-lg mt-4 hover:bg-blue-700 transition-colors" onClick={closeModal}>Accept It</button>
+        <h3 className="font-black underline text-white text-lg my-3">Remove Course</h3>
+        <p className="text-sm mb-4">Are you sure you want to remove this course? This action cannot be undone.</p>
+        <div className="flex justify-center space-x-4 mt-4">
+          <button className="bg-red-500 text-white py-2 px-4 rounded-lg hover:bg-red-700 transition-colors font-bold" onClick={closeModal}>
+            Remove
+          </button>
+          <button className="bg-gray-500 text-white py-2 px-4 rounded-lg hover:bg-gray-700 transition-colors font-bold" onClick={closeModal}>
+            Cancel
+          </button>
+        </div>
       </div>
     </div>,
     document.querySelector('.myPortalModalDiv')
-  )
-}
+  );
+};
 
-export default RemoveCourse
+export default RemoveCourse;
