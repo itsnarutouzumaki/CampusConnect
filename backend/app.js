5d24e1a94c97f01405f7801fe66e33db297d6eac
qrquf <siddharthsrivastava117@gmail.com>
2025-02-05 05:29:54 -0800

    added student login and signup

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,11 @@ const express=require('express');
 const app=express();
 const bodyParser=require('body-parser');
 const cors=require('cors');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 const routes=require('./routes/index.js');
 app.use(bodyParser.json());
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true })); // Add this line to parse URL-encoded data
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
--- backend/controller/studentcontroller/studentregistrationandsignup.js
+++ backend/controller/studentcontroller/studentregistrationandsignup.js
@@ -2,11 +2,11 @@ const multer=require('multer');
 const item=require('../../models/studentschema.js');
 const bcrypt=require('bcrypt');
 const jwt=require('jsonwebtoken');
+
 const checkUserExists = async (req, res, next) => {
     const { email } = req.body;
-
     try {
-        const student = await item.findOne({ email });
+    const student = await item.findOne({ email });
         if (item) {
             return res.status(400).send('User already exists');
         }
@@ -18,14 +18,20 @@ const checkUserExists = async (req, res, next) => {
 const signup=async (req,res)=>
 {
 const {fullname,email,password}=req.body;
-const hashedPassword=await bcrypt.hash(password,10);
-const data=new item({
-    "fullname":fullname,
-    "email":email,
-    "password":hashedPassword
-});
+const hashedPassword=await bcrypt.hash(req.body.password,10,
+    (err, hash) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log('Hashed Password:', hash);
+        }
+      }
+);
+
+//req.body.password=hashedPassword;
+const data=new item(req.body);
 const saveddata=await data.save();
-const token = jwt.sign({ _id: saveddata._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+const token = jwt.sign({ _id: saveddata._id }, "aa12aa3aa4", { expiresIn: '1h' });
 res.send(saveddata);
 };
 const login=async (req,res)=>
@@ -43,7 +49,6 @@ if(!isMatch)
 }
 const token = jwt.sign({ _id: item._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
 res.send({ item, token });
-
 }
 module.exports={
     signup,
