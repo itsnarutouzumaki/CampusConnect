65bfcbcf7edff025eecdd82da83a859e9e9843f2
somenath singh <144797336+itsnarutouzumaki@users.noreply.github.com>
2025-03-07 06:57:30 +0530

    Update Logo.jsx

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -4,7 +4,7 @@ import "./Logo.css"
 function Logo() {
   return (
     <div className="logo-container">
-     <img src="../../public/image.png" alt="logo" className='logo-image'/>
+     <img src="../../image.png" alt="logo" className='logo-image'/>
     </div>
   )
 }
